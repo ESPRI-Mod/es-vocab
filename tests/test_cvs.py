d478eed6e20cc7a5ccd03738b31b32e03715a7b1
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2024-07-22 11:54:28 +0200

    add pytest

--- tests/test_cvs.py
+++ tests/test_cvs.py
@@ -7,11 +7,13 @@ def test_universe_and_project(caplog):
 
     # Test universe parsing first and stop in any case of error to avoid side effects with project parsing.
     cvs.TERMS_OF_UNIVERSE = cvs._parse_terms_of_universe(settings.DATA_DESCRIPTORS_PARENT_DIR_PATH)
+    print(cvs.TERMS_OF_UNIVERSE) 
     assert (cvs.TERMS_OF_UNIVERSE is not None) and (len(cvs.TERMS_OF_UNIVERSE) > 0)
     count_error_tags = caplog.text.count("ERROR")
     assert count_error_tags == 0
 
     cvs.TERMS_OF_COLLECTIONS_OF_PROJECTS = cvs._parse_projects(settings.PROJECTS_PARENT_DIR_PATH)
+    print(cvs.TERMS_OF_COLLECTIONS_OF_PROJECTS)
     assert (cvs.TERMS_OF_COLLECTIONS_OF_PROJECTS is not None) and (len(cvs.TERMS_OF_COLLECTIONS_OF_PROJECTS) > 0)
     count_error_tags = caplog.text.count("ERROR")
     assert count_error_tags == 0
