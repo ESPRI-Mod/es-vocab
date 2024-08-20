88f92c1e1b889ea790493f1e736dffe192cc587d
Sébastien Gardoll <sebastien@gardoll.fr>
2024-08-20 11:54:52 +0200

    fix mypy errors

--- src/es_vocab/db/cvs.py
+++ src/es_vocab/db/cvs.py
@@ -2,6 +2,7 @@ import importlib.util
 import inspect
 import json
 import logging
+import typing
 from pathlib import Path
 
 from pydantic import BaseModel
@@ -38,23 +39,32 @@ def get_term_in_project_collection(project_id: str, collection_id: str, term_id:
 ######################### PARSING FUNCTIONS #########################
 
 
-def _get_classes_from_module(module) -> dict[str, type]:
+def _get_classes_from_module(module) -> dict[str, BaseModel]:
     # inspect.getmembers gets all classes defined in the module.
     classes = inspect.getmembers(module, inspect.isclass)
     # Filter out classes that are not defined in this module
-    module_classes = {name: cls for name, cls in classes if cls.__module__ == module.__name__}
+    module_classes: dict[str, BaseModel] = dict()
+    for name, cls in classes:
+        if cls.__module__ == module.__name__ and issubclass(cls, BaseModel):
+            module_classes[name] = typing.cast(BaseModel, cls)
     return module_classes
 
 
-def _load_pydantic_class(pydantic_module_file_path: Path) -> type | None:
+def _load_pydantic_class(pydantic_module_file_path: Path) -> BaseModel | None:
     result = None
     class_name = settings.from_pydantic_module_filepath_to_pydantic_class_name(pydantic_module_file_path)
     try:
         spec = importlib.util.spec_from_file_location(name=class_name, location=pydantic_module_file_path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        classes = _get_classes_from_module(module)
-        result = classes[class_name]
+        if spec is not None:
+            module = importlib.util.module_from_spec(spec)
+            if spec.loader is not None:
+                spec.loader.exec_module(module)
+                classes = _get_classes_from_module(module)
+                result = classes[class_name]
+            else:
+                _LOGGER.error(f"loader for {class_name} is not found")
+        else:
+            _LOGGER.error(f"class spec {class_name} is not found")
     except FileNotFoundError:
         _LOGGER.error(f"file {pydantic_module_file_path} not found")
     except KeyError:
@@ -63,7 +73,7 @@ def _load_pydantic_class(pydantic_module_file_path: Path) -> type | None:
 
 
 def _parse_terms_of_universe(universe_dir_path: Path) -> dict[str, dict[str, BaseModel]]:
-    result = dict()
+    result: dict[str, dict[str, BaseModel]] = dict()
     for data_descriptor_dir_path in universe_dir_path.iterdir():
         if data_descriptor_dir_path.stem[0] == ".":
             continue
@@ -84,14 +94,14 @@ def _parse_terms_of_universe(universe_dir_path: Path) -> dict[str, dict[str, Bas
             try:
                 json_content_file = Path(term_file_path).read_text()
                 pydantic_instance = pydantic_class.model_validate_json(json_content_file)
-                result[data_descriptor_dir_path.name][pydantic_instance.id] = pydantic_instance
+                result[data_descriptor_dir_path.name][pydantic_instance.id] = pydantic_instance  # type: ignore
             except Exception as e:
                 _LOGGER.error(f"unable to parse {term_file_path}:\n{str(e)}")
     return result
 
 
-def _parse_collections_of_project(project_dir_path: Path) -> dict[str : dict[str:BaseModel]]:
-    result = dict()
+def _parse_collections_of_project(project_dir_path: Path) -> dict[str, dict[str, BaseModel]]:
+    result: dict[str, dict[str, BaseModel]] = dict()
     for collection_file_path in project_dir_path.glob("*.json"):
         _LOGGER.debug(f"parse collection: {collection_file_path}")
         collection_id = settings.from_collection_file_path_to_collection_id(collection_file_path)
@@ -114,7 +124,7 @@ def _parse_collections_of_project(project_dir_path: Path) -> dict[str : dict[str
     return result
 
 
-def _parse_projects(parent_projects_dir_path: Path) -> dict[str : dict[str : dict[str:BaseModel]]]:
+def _parse_projects(parent_projects_dir_path: Path) -> dict[str, dict[str, dict[str, BaseModel]]]:
     result = dict()
     for project_dir_path in parent_projects_dir_path.iterdir():
         _LOGGER.debug(f"parse collections of project: {project_dir_path}")
@@ -127,13 +137,13 @@ def _parse_projects(parent_projects_dir_path: Path) -> dict[str : dict[str : dic
 ######################### DICTIONARIES #########################
 
 # dict[data_descriptor_id: dict[term id, term object]
-TERMS_OF_UNIVERSE: dict[str, dict[str, BaseModel]] = None
+TERMS_OF_UNIVERSE: dict[str, dict[str, BaseModel]]
 
 # dict[project_id: dict[collection_id: dict[term_id: term object]]]
-TERMS_OF_COLLECTIONS_OF_PROJECTS: dict[str : dict[str : dict[str:BaseModel]]] = None
+TERMS_OF_COLLECTIONS_OF_PROJECTS: dict[str, dict[str, dict[str, BaseModel]]]
 
 # dict[data_descriptor_id: class]
-DATA_DESCRIPTOR_CLASS: dict[str:type] = dict()
+DATA_DESCRIPTOR_CLASS: dict[str, BaseModel] = dict()
 
 
 # Singleton
