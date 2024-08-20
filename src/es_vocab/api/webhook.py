16c0e7205900654cb90156689a8cfc86055769c8
Sébastien Gardoll <sebastien@gardoll.fr>
2024-08-20 14:29:41 +0200

    use fastapi status enum as http returned codes

--- src/es_vocab/api/webhook.py
+++ src/es_vocab/api/webhook.py
@@ -2,7 +2,7 @@ import hashlib
 import hmac
 import json
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, status
 
 from es_vocab.utils.settings import SECRET_TOKEN
 
@@ -15,18 +15,18 @@ def verify_signature(request_body: bytes, headers):
     signature_256 = headers.get("X-Hub-Signature-256")
 
     if not signature_256:
-        raise HTTPException(status_code=400, detail="Missing signature")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Missing signature")
 
     # The cryptographic signature must be SHA-256.
     sha_name, signature = signature_256.split("=")
 
     if sha_name != "sha256":
-        raise HTTPException(status_code=400, detail="Unsupported signature type")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Unsupported signature type")
     # Create a new HMAC digester using the secret token and SHA-256.
     mac = hmac.new(SECRET_TOKEN.encode(), msg=request_body, digestmod=hashlib.sha256)
     # Compare the signatures.
     if not hmac.compare_digest(mac.hexdigest(), signature):
-        raise HTTPException(status_code=400, detail="Invalid signature")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid signature")
 
 
 @router.post("/repoupdate", include_in_schema=False)
@@ -39,7 +39,7 @@ async def handle_webhook(request: Request):
     # Verify branch main ? TODO: explicite this comment.
     payload = json.loads(body)
     if not payload.get("ref") == "refs/heads/main":
-        raise HTTPException(status_code=501, detail="don't push in main")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="push in main is not allowed")
 
     try:
         with open("/update/havetorestart", "w") as f:
@@ -47,4 +47,6 @@ async def handle_webhook(request: Request):
 
         return {"status": "success", "message": "Webhook received and verified"}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Script failed with error: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Script failed with error: {str(e)}"
+        )
