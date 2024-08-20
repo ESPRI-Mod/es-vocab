ebbf2279cbf6f07f1e75ee88b511afb3abc8e64f
Sébastien Gardoll <sebastien@gardoll.fr>
2024-08-20 14:09:33 +0200

    minor reformatting

--- src/es_vocab/api/sparql.py
+++ src/es_vocab/api/sparql.py
@@ -1,6 +1,5 @@
-from rdflib_endpoint import SparqlRouter
 from rdflib import Graph
-import es_vocab.db.cvs as cvs
+from rdflib_endpoint import SparqlRouter
 
 import es_vocab.utils.settings as settings
 
@@ -12,8 +11,8 @@ sparql_router = SparqlRouter(
     graph=g,
     path="/sparql",
     # Metadata used for the SPARQL service description and Swagger UI:
-    title="SPARQL endpoint for es-vocab RDFLib graph ",
+    title="SPARQL endpoint for es-vocab RDFLib graph",
     description="A SPARQL endpoint to serve machine learning models, or any other logic implemented in Python. \n[Source code](https://github.com/vemonet/rdflib-endpoint)",
     version="0.1.0",
-    public_url='http://es-vocab.ipsl.fr/sparql',
+    public_url="http://es-vocab.ipsl.fr/sparql",
 )
--- src/es_vocab/api/webhook.py
+++ src/es_vocab/api/webhook.py
@@ -1,25 +1,26 @@
-from fastapi import APIRouter, Request, HTTPException
-import hmac
 import hashlib
-from es_vocab.utils.settings import SECRET_TOKEN
+import hmac
 import json
+
+from fastapi import APIRouter, HTTPException, Request
+
+from es_vocab.utils.settings import SECRET_TOKEN
 
 router = APIRouter(prefix="/webhook")
 # Replace 'your-secret-token' with the actual secret token you entered in the GitHub webhook settings
 
 
-
 def verify_signature(request_body: bytes, headers):
     # Extract the signature from the headers
-    signature_256 = headers.get('X-Hub-Signature-256')
-    
+    signature_256 = headers.get("X-Hub-Signature-256")
+
     if not signature_256:
         raise HTTPException(status_code=400, detail="Missing signature")
 
     # The signature will be of the format "sha256=..."
-    sha_name, signature = signature_256.split('=')
-    
-    if sha_name != 'sha256':
+    sha_name, signature = signature_256.split("=")
+
+    if sha_name != "sha256":
         raise HTTPException(status_code=400, detail="Unsupported signature type")
     # Create a new HMAC digester using the secret token and SHA256
     mac = hmac.new(SECRET_TOKEN.encode(), msg=request_body, digestmod=hashlib.sha256)
@@ -27,26 +28,25 @@ def verify_signature(request_body: bytes, headers):
     if not hmac.compare_digest(mac.hexdigest(), signature):
         raise HTTPException(status_code=400, detail="Invalid signature")
 
-@router.post("/repoupdate",include_in_schema=False)
+
+@router.post("/repoupdate", include_in_schema=False)
 async def handle_webhook(request: Request):
     body = await request.body()
-    
+
     # Verify the signature
     verify_signature(body, request.headers)
 
     # Verify branch main ?
     payload = json.loads(body)
-    if not payload.get('ref') == 'refs/heads/main':
+    if not payload.get("ref") == "refs/heads/main":
         raise HTTPException(status_code=501, detail="push not on main")
-
 
     try:
         with open("/maj/havetorestart", "w") as f:
             f.write("go")
 
-        return {"status": "success"}  
+        return {"status": "success"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Script failed with error: {e}")
 
-     
     return {"status": "success", "message": "Webhook received and verified"}
