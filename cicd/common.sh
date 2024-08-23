eab159ce77c9c98074c6d4ff35706d3b67333848
Sébastien Gardoll <sebastien@gardoll.fr>
2024-08-23 16:55:42 +0200

    initial import

---
+++ cicd/common.sh
@@ -0,0 +1,4 @@
+readonly UPDATE_FILE_PATH="${SCRIPT_DIR_PATH}/../../update"
+readonly DOCUMENTATION_DIR_PATH="${SCRIPT_DIR_PATH}/../documentation"
+readonly MKDOCS_SETTINGS_FILE_PATH="${DOCUMENTATION_DIR_PATH}/mkdocs.yml"
+readonly GIT_DEPLOYMENT_BRANCH_NAME='main'
