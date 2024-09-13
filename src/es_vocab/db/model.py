154a1b4839305028221e07c17b7c924c6429e1f2
Sébastien Gardoll <sebastien@gardoll.fr>
2024-09-13 16:59:38 +0200

    composite part can be term, pattern xor composite

--- src/es_vocab/db/model.py
+++ src/es_vocab/db/model.py
@@ -1,6 +1,10 @@
-import sqlalchemy as sa
+from typing import Any
+
+from pydantic import model_validator
+from pydantic_core import PydanticCustomError, ValidationError, InitErrorDetails
 from sqlmodel import SQLModel, Field, Relationship
 from sqlalchemy.dialects.sqlite import JSON
+
 
 class PkMixin:
     pk: int | None = Field(default=None, primary_key=True)
@@ -52,11 +56,37 @@ class UniverseTermComposite(Term, table=True):
 
 
 class UniverseCompositePart(SQLModel, PkMixin, IdMixin, table=True):
-    __tablename__ = "composite_parts"
+    __tablename__ = "u_composite_parts"
     rank: int
     is_required: bool
-    composite_pk: int | None = Field(default=None, foreign_key="u_term_composites.pk")
-    composite: UniverseTermComposite = Relationship(back_populates="parts")
+    parent_composite_pk: int | None = Field(default=None, foreign_key="u_term_composites.pk")
+    parent_composite: UniverseTermComposite = Relationship(back_populates="parts")
+    
+    u_term_pk: int | None = Field(default=None, foreign_key="u_terms.pk")
+    u_term: UniverseTerm = Relationship()
+    u_term_pattern_pk: int | None = Field(default=None, foreign_key="u_term_patterns.pk")
+    u_term_pattern: UniverseTermPattern = Relationship()
+    u_term_composite_pk: int | None = Field(default=None, foreign_key="u_term_composites.pk")
+    u_term_composite: UniverseTermComposite = Relationship()
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_xor(cls, data: dict[str, Any]):
+        no_term = data.get("u_term_pk") is None
+        no_term_pattern = data.get("u_term_pattern_pk") is None
+        no_term_composite = data.get("u_term_composite_pk") is None
+        if (no_term and no_term_pattern and no_term_composite) or \
+           (not no_term and no_term_pattern and no_term_composite) or \
+           (no_term and not no_term_pattern and no_term_composite) or \
+           (no_term and no_term_pattern and not no_term_composite) :
+            errors = []
+            for loc in ["u_term_pk", "u_term_pattern_pk", "u_term_composite_pk"]:
+                errors.append(\
+                    InitErrorDetails(type=PydanticCustomError("xor error", "exactly one an only one term is required"),
+                                     loc=tuple(loc),
+                                     input= data))
+            raise ValidationError.from_exception_data(cls.__name__, errors)
+        return data
 
 
 class Project(SQLModel, PkMixin, IdMixin, VersionMixin, table=True):
