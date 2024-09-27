09d38b3b8d4baa63bdd1c2c852b4440dbb2d4afc
Sébastien Gardoll <sebastien@gardoll.fr>
2024-09-27 16:23:36 +0200

    remove obsolete commented instructions

--- src/es_vocab/utils/server.py
+++ src/es_vocab/utils/server.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 import es_vocab.api.sparql as spq
 import es_vocab.api.urls as urls
@@ -31,7 +30,6 @@ def create_app() -> FastAPI:
     app.include_router(api_app.router)
     app.include_router(spq.sparql_router)
     app.include_router(webhook.router)
-    #app.mount("/", StaticFiles(directory="documentation/site", html=True), name="site") # not needed anymore serve on github.io (https://espri-mod.github.io/es-vocab/website/)
     return app
 
 
